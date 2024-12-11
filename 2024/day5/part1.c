71147f14d20ad5f9aa2ad431a8986f3525ef7346
Ayilay <georgetroulis@gmail.com>
2024-12-11 07:36:05 -0800

    day5

---
+++ 2024/day5/part1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+
+#include "problem_solver.h"
+
+//--------------------------------------------------------------------------------
+//  Private State
+//--------------------------------------------------------------------------------
+
+
+//--------------------------------------------------------------------------------
+//  API Functions
+//--------------------------------------------------------------------------------
+
+void SOLVER_Init( void* arg )
+{
+}
+
+void SOLVER_ProcessLine( char* line )
+{
+    rmnewl( line );
+}
+
+void SOLVER_PrintSolution( void )
+{
+}
